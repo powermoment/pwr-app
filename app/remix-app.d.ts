82b52ac652effb66240dc442911800c386549ecf
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2022-06-14 09:42:57 +0300

    Update user types

--- app/remix-app.d.ts
+++ app/remix-app.d.ts
@@ -1,10 +1,18 @@
 import type { Session, User } from "@supabase/supabase-js";
 import type { GitHubProfile } from "remix-auth-github";
 
+export type DBUser = {
+  id: string;
+  email: string;
+  username: string | null;
+  avatar_url: string | null;
+  updated_at: string;
+};
+
 export type AuthenticatedUser = {
   user: Session | User | null;
   profile?: GitHubProfile;
-  data: any;
+  data: { id: string };
 };
 
 declare global {
