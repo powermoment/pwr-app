a63e7ebef949354188f761730dc6cb6e690949c4
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2022-06-14 09:51:02 +0300

    Done with insert cheks action

--- app/routes/checks.tsx
+++ app/routes/checks.tsx
@@ -1,15 +1,37 @@
+import type { ActionFunction } from "@remix-run/node";
 import { Form } from "@remix-run/react";
-import React from "react";
+import { authenticator } from "~/services/auth.server";
+import { supabase } from "~/services/supabase.server";
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+  const title = form.get("title");
+  const body = form.get("body");
+
+  const user = await authenticator.isAuthenticated(request);
+
+  const { data, error } = await supabase
+    .from("checks")
+    .insert({ title, body, author: user?.data?.id });
+
+  console.log(data, error);
+
+  return null;
+};
 
 const checks = () => {
   return (
-    <Form>
-      <input
-        type="text"
-        placeholder="Type here"
-        className="input input-bordered input-primary w-full max-w-xs"
-      />
-    </Form>
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <Form method="post">
+        <input
+          type="text"
+          placeholder="Title"
+          className="input input-bordered input-primary w-full max-w-xs"
+        />
+        <textarea className="textarea" placeholder="Body"></textarea>
+        <button>Send check</button>
+      </Form>
+    </div>
   );
 };
 
