a6613736edaf80946ccb9911102bf93f67eb54e5
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2022-06-15 11:37:56 +0300

    Add checks page and create check page

---
+++ app/routes/checks/index.tsx
@@ -0,0 +1,14 @@
+const checks = () => {
+  return (
+    <div className="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 lg:px-8">
+      <div className="relative p-8 text-center border border-gray-200 rounded-lg">
+        <h2 className="text-2xl font-medium">There's nothing here...</h2>
+        <p className="mt-4 text-sm text-gray-500">
+          Created checkouts will appear here, try creating one!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default checks;
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -12,10 +12,6 @@ export default function Index() {
   return (
     <div className="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 lg:px-8">
       <div className="relative p-8 text-center border border-gray-200 rounded-lg">
-        <h2 className="text-2xl font-medium">There's nothing here...</h2>
-        <p className="mt-4 text-sm text-gray-500">
-          Created checkouts will appear here, try creating one!
-        </p>
         <code className="mt-4 block break-words whitespace-normal">
           {JSON.stringify(data)}
         </code>
