ca7596b3bf453371f7b642a488f32f2071246a39
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2022-06-17 09:41:27 +0300

    Fix svg icons

--- app/components/HeaderMenu.tsx
+++ app/components/HeaderMenu.tsx
@@ -57,11 +57,11 @@ const HeaderMenu = ({ user }: HeaderMenuProps) => {
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
-            stroke-width="2"
+            strokeWidth="2"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
             />
           </svg>
@@ -77,11 +77,11 @@ const HeaderMenu = ({ user }: HeaderMenuProps) => {
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
-            stroke-width="2"
+            strokeWidth="2"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
             />
           </svg>
