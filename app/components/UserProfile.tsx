5b91932bd85bd670e595806ea444019c3b8f2b49
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2022-06-18 00:04:32 +0300

    Create Avatar component

--- app/components/UserProfile.tsx
+++ app/components/UserProfile.tsx
@@ -2,32 +2,11 @@ import React from "react";
 import { Menu, Transition } from "@headlessui/react";
 import { Fragment } from "react";
 import type { AuthenticatedUser } from "~/remix-app";
+import Avatar from "./Avatar";
 
 type Props = {
   user?: AuthenticatedUser;
 };
-
-const Avatar = ({ user }: { user?: AuthenticatedUser }) =>
-  user?.data?.avatar_url ? (
-    <img
-      className="h-10 w-10 rounded-full object-cover"
-      src={user?.data?.avatar_url}
-      alt={user?.data?.username || "User avatar"}
-    />
-  ) : (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-10 w-10 text-gray-500"
-      viewBox="0 0 20 20"
-      fill="currentColor"
-    >
-      <path
-        fillRule="evenodd"
-        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
-        clipRule="evenodd"
-      />
-    </svg>
-  );
 
 const Info = ({ user }: { user?: AuthenticatedUser }) => (
   <p className="ml-2 hidden text-left text-xs md:block">
