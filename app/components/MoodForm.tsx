4592db7e46d9721fba5468c9ed0372dfa237659d
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2022-06-19 17:54:42 +0300

    Add submit button to MoodForm

--- app/components/MoodForm.tsx
+++ app/components/MoodForm.tsx
@@ -10,14 +10,33 @@ export const links: LinksFunction = () => {
   return [...moodSliderLinks(), ...woobleStyles()];
 };
 
-const MoodForm = () => {
-  const [moodValue, setMoodValue] = useState<string>();
+type MoodFormProps = {
+  onChange: (value: string) => void;
+};
+
+const DEFAULT_VALIE = "3";
+
+const MoodForm = ({ onChange }: MoodFormProps) => {
+  const [moodValue, setMoodValue] = useState<string>(DEFAULT_VALIE);
+
   const handleMoodChange = (value: string) => setMoodValue(value);
+  const handleSubmit = () => onChange(moodValue);
 
   return (
-    <div className="flex flex-col space-y-8">
+    <div className="flex flex-col space-y-16">
       <Wooble color={getMoodColorByValue(moodValue)} />
-      <MoodSlider onChange={handleMoodChange} />
+      <MoodSlider value={moodValue} onChange={handleMoodChange} />
+      <div className="flex">
+        <button
+          onClick={handleSubmit}
+          className="group relative inline-block w-full text-sm font-medium text-pwr focus:outline-none focus:ring active:text-pwr"
+        >
+          <span className="absolute inset-0 translate-x-0 translate-y-0 bg-pwr transition-transform group-hover:translate-y-0.5 group-hover:translate-x-0.5"></span>
+          <span className="relative block border border-current bg-white px-8 py-3 text-pwr">
+            Оценить
+          </span>
+        </button>
+      </div>
     </div>
   );
 };
