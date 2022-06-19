8231753a17f03050022d47244a176e07c8c73924
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2022-06-19 19:16:15 +0300

    Update ReasonField

--- app/components/MoodForm/ReasonsField.tsx
+++ app/components/MoodForm/ReasonsField.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import { useLoaderData } from "@remix-run/react";
+import ReasonsSelect from "../ReasonsSelect";
 
 const ReasonsField = () => {
+  const { reasons } = useLoaderData();
+
   return (
-    <div className="rounded-lg border border-gray-200 p-6 text-center">
-      <strong className="rounded-full border border-red-500 bg-red-100 px-5 py-1.5 text-[10px] uppercase tracking-wide text-red-500">
-        Bug
-      </strong>
+    <div className="flex flex-col space-y-6 rounded-lg border border-gray-200 p-6 text-center">
+      <div className="flex flex-wrap gap-4">
+        <ReasonsSelect reasons={reasons} />
+      </div>
       <div className="flex">
         <button
           type="submit"
@@ -13,7 +16,7 @@ const ReasonsField = () => {
         >
           <span className="absolute inset-0 translate-x-0 translate-y-0 bg-pwr transition-transform group-hover:translate-y-0.5 group-hover:translate-x-0.5"></span>
           <span className="relative block border border-current bg-white px-8 py-3 text-pwr">
-            Оценить
+            Отправить
           </span>
         </button>
       </div>
