7b9f4bb6f56cca51a15a0fd07da3a20ada2af586
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2022-06-19 23:10:07 +0300

    Remove login link from HeaderMenu

--- app/components/HeaderMenu.tsx
+++ app/components/HeaderMenu.tsx
@@ -15,7 +15,7 @@ const HeaderMenu = ({ user }: HeaderMenuProps) => {
           className="group relative inline-block focus:outline-none focus:ring"
           to="/checks/create"
         >
-          <span className="bg-pwr absolute inset-0 translate-x-1.5 translate-y-1.5  transition-transform group-hover:translate-y-0 group-hover:translate-x-0"></span>
+          <span className="absolute inset-0 translate-x-1.5 translate-y-1.5 bg-pwr  transition-transform group-hover:translate-y-0 group-hover:translate-x-0"></span>
 
           <span className="relative inline-block border-2 border-current px-3 py-2 text-sm font-bold uppercase tracking-widest text-black group-active:text-opacity-75">
             Check!
@@ -45,28 +45,6 @@ const HeaderMenu = ({ user }: HeaderMenuProps) => {
           <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
         </svg>
       </Link>
-
-      {!user?.data && (
-        <Link
-          to="/login"
-          className="block shrink-0 rounded-lg bg-white p-2.5 text-gray-600 shadow-sm hover:text-gray-700"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth="2"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
-            />
-          </svg>
-        </Link>
-      )}
     </div>
   );
 };
