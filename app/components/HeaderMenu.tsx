757864a2ad2dd82a68199d026cf48121194fcd7d
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2022-06-19 17:32:53 +0300

    Update colors ad HeaderMenu

--- app/components/HeaderMenu.tsx
+++ app/components/HeaderMenu.tsx
@@ -15,7 +15,7 @@ const HeaderMenu = ({ user }: HeaderMenuProps) => {
           className="group relative inline-block focus:outline-none focus:ring"
           to="/checks/create"
         >
-          <span className="absolute inset-0 translate-x-1.5 translate-y-1.5 bg-yellow-300 transition-transform group-hover:translate-y-0 group-hover:translate-x-0"></span>
+          <span className="bg-pwr absolute inset-0 translate-x-1.5 translate-y-1.5  transition-transform group-hover:translate-y-0 group-hover:translate-x-0"></span>
 
           <span className="relative inline-block border-2 border-current px-3 py-2 text-sm font-bold uppercase tracking-widest text-black group-active:text-opacity-75">
             Check!
